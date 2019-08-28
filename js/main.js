701c3d60f2da893cd77eb3edb4abf71353f09488
Alex <o.a.momotyuk@gmail.com>
2019-08-27 22:20:33 -0500

    added info comments

---
+++ js/main.js
@@ -0,0 +1,30 @@
+/*
+    You have volunteered your time to a local political candidate, Elizabeth Sanger, who wants to become a US representative in Congress for your district. Unfortunately, the team discovered that you're a software developer, so they have begged you to build an application that lets them track volunteers, and store information about Elizabeth and her campaign.
+
+    Your job is to define the different objects and arrays, their structure, and the corresponding properties for each, to represent the following information about Elizabeth's campaign.
+*/
+/*
+    1. Her congressional district (you can use yours here)
+    2. Her platform statements for the following issues.
+        Taxes
+        Jobs
+        Infrastructure
+        Health care
+        Crime and enforcement
+    3. URL for donation form
+    4. Calendar of events
+    5. Volunteer information
+        Name
+        Address
+        Email
+        Phone number
+        Availability
+        What activities each one is willing to do (e.g. answering phone calls, taking polls, etc.)
+    6. Biography
+    7. Image gallery
+        Head shot
+        Picture of family
+        Picture of constituents
+    8. Mission statement
+    9. URL for registering to vote
+*/
